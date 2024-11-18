2a1c0416e4bac2ef72dabbf2eaac0f099114e71c
murugappankrishna <murugappankrishna@icloud.com>
2024-11-18 22:00:07 +0530

    initial commit

---
+++ src/main/java/com/murugappan/LearningSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.murugappan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringDataJpaApplication.class, args);
+    }
+
+}
